6dbfdcd07e317bbc1390e199de88b187fe5c2d57
Nikhiltripathi90 <47690425+Nikhiltripathi90@users.noreply.github.com>
2019-04-25 22:29:57 +0530

    Create Modifyme.sh

---
+++ Modifyme.sh
@@ -0,0 +1,15 @@
+#!/data/data/com.termux/usr/files/bin/bash
+mpv TTS.mp3
+clear
+toilet Hacker | lolcat
+sleep 1
+clear
+cowsay -f eyes Tfnikhil | lolcat
+toilet -f standard -F gay "Nikhil"
+sleep 1
+clear
+echo
+neofetch
+echo -e '\e[1;93m'
+date
+echo
