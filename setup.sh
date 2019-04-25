687634cd10d88693658f66acabd0f0c5ac31cd81
Nikhiltripathi90 <47690425+Nikhiltripathi90@users.noreply.github.com>
2019-04-25 22:26:26 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/data/data/com.termux/usr/files/bin/bash
+apt-get update
+apt-get upgrade
+apt-get install python
+apt-get install python2
+apt-get install figlet
+apt-get install toilet
+apt-get install cowsay
+pip2 install lolcat
+apt-get install mpv
+apt-get install neofetch
+clear
+sleep 2
+figlet -f big Done
+echo
+echo "Script by Nikhil" | lolcat
+echo
+echo "Don't forget to subscribe my channel" | lolcat
+echo
+echo "Now you can run the script" | lolcat
